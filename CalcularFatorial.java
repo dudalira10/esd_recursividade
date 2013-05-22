91017501e071853a6457280b85c9b1c522375c7b
carlos_lira <dudaaurora2007@gmail.com>
2013-05-22 15:50:45 -0300

    Código recursividade

---
+++ CalcularFatorial.java
@@ -0,0 +1,19 @@
+public class CalcularFatorial{
+
+	public static int fatorial(int num){
+		if(num == 0){
+			return 1;
+		}
+		else
+			return num * fatorial(num - 1);
+	}
+	
+	public static void main(String args[]){
+			int aux = Integer.parseInt(args[0]);
+			System.out.println(fatorial(aux));
+			
+		
+	}
+
+
+}
---
+++ CalcularFibonacci.java
@@ -0,0 +1,19 @@
+public class CalcularFibonacci{
+		
+		public static int fibonacci(int num){
+			if((num == 1) || (num == 2)){
+				return 1;
+			}
+			else
+				return (fibonacci(num - 1) + fibonacci(num - 2));		
+		}
+		
+		public static void main(String args[]){
+			int aux = Integer.parseInt(args[0]);
+			System.out.println(fibonacci(aux));
+			
+		}
+		
+
+
+}
